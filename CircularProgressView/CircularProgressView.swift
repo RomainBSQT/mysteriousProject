36df289859670feed57e1dd7ca4b0debf4aa7a1d
Romain Bousquet <rom.bousqu@gmail.com>
2017-01-30 21:08:18 +0100

    Initial commit

---
+++ CircularProgressView/CircularProgressView.swift
@@ -0,0 +1,23 @@
+//
+//  CircularProgressView.swift
+//  CircularProgressView
+//
+//  Created by Romain Bousquet on 30/01/2017.
+//  Copyright © 2017 Romain Bousquet. All rights reserved.
+//
+
+import UIKit
+
+class CircularProgressView: UIView {
+
+	
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+}
